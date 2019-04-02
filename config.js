41f7631407ab6f5a574d57549b2bccdc872748ec
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-04-02 12:30:23 +0200

    25.8

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID':'15993180149-oikb1qupeab0mnihu5h5qh52b2bibkv0.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET':'idNQGCXMez9iqRor_Cppb82a',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
